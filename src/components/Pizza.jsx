5783e8346fc171110a3366e229c1b587e299ab9c
yonathandevpro <yonathanber@icloud.com>
2024-07-08 14:49:05 +0300

    Created Pizza component

---
+++ src/components/Pizza.jsx
@@ -0,0 +1,9 @@
+export default function Pizza() {
+  return (
+    <div>
+      <img src="../../public/pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
