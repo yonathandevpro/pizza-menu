4158bddaf89b2f137fbf42a20e8b08ddfe0ab885
yonathandevpro <yonathanber@icloud.com>
2024-07-10 11:51:07 +0300

    Added multiple return on pizza component

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,4 +1,6 @@
 export default function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt="Pizza Spinaci" />
